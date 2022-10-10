f4fc88c50bac42723ff0053191741fcfdac49ab6
chandnidevani <chandnidevani5555@gmail.com>
2022-10-10 09:00:30 +0530

    jewellery

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import Footer from './components/footer';
+import Header from './components/header';
+import Slider from './components/slider';
+import Gallery from './components/gallery';
+import Product from './components/product';
+import Feature from './components/feature';
+import Categories from './components/categories';
+import Blog from './components/blog';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Footer/>
+      <Slider/>
+      <Gallery/>
+      <Product/>
+      <Feature/>
+      <Categories/>
+      <Blog/>
     </div>
   );
 }
